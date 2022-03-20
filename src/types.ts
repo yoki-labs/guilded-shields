6af4692648a2d40c17df264b6ffbc4d975854d21
zaida04 <nico.03727+github@gmail.com>
2022-03-20 19:19:49 -0400

    chore: prettier

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,4 @@
-import type {
-    RequestGenericInterface,
-    FastifyReply,
-    FastifyRequest,
-} from "fastify";
+import type { RequestGenericInterface, FastifyReply, FastifyRequest } from "fastify";
 import type { Server, IncomingMessage, ServerResponse } from "http";
 
 export interface GuildedInviteResponse {
@@ -25,28 +21,13 @@ export interface VanityInviteInfo {
     };
 }
 
-export type badgeStyle =
-    | "plastic"
-    | "flat"
-    | "flat-square"
-    | "for-the-badge"
-    | "social";
+export type badgeStyle = "plastic" | "flat" | "flat-square" | "for-the-badge" | "social";
 
 // Request object extendable with a route interface like defined above, that way you have fully typed headers, body, queryparams
-export type Request<T = RequestGenericInterface> = FastifyRequest<
-    T,
-    Server,
-    IncomingMessage
->;
+export type Request<T = RequestGenericInterface> = FastifyRequest<T, Server, IncomingMessage>;
 
 // Response object extendable with a route interface like defined above
-export type Response = FastifyReply<
-    Server,
-    IncomingMessage,
-    ServerResponse,
-    RequestGenericInterface,
-    unknown
->;
+export type Response = FastifyReply<Server, IncomingMessage, ServerResponse, RequestGenericInterface, unknown>;
 
 export type BadgeGetReq = Request<{
     Querystring: { color?: string; style?: string; isVanity?: boolean };
